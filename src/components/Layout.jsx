6a9e5167aa04f0620331b8c3d339e3e8cbb49c61
adisk01 <adiscrazy01@gmail.com>
2023-12-02 21:57:52 +0530

    Select and Delete Done

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,6 +4,8 @@ import ReactPaginate from 'react-paginate';
 import './Layout.css'
 const Layout = (props) => {
     const { data } = props;
+    const alldata=data;
+    console.log("render" , alldata);
     const [currentItems, setCurrentItems] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     const [itemOffset, setItemOffset] = useState(0);
@@ -11,25 +13,38 @@ const Layout = (props) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [role, setRole] = useState("");
-   const itemsPerPage=10;
-    const handleCheckboxChange = async (id) => {};
+    const [selectedItems, setSelectedItems] = useState([]);
+    const itemsPerPage=10;
+    const handleCheckboxChange = () => {
+      // If all items are already selected, clear the selection
+      if (selectedItems.length === currentItems.length) {
+        setSelectedItems([]);
+      } else {
+        // Otherwise, select all items
+        const allItemIds = currentItems.map(item => item.id);
+        setSelectedItems(allItemIds);
+      }
+    };
   
-    const handleEdit = async (id) => {
+    const handleEdit = (id) => {
       const editItem = data.find((item) => item.id === id);
   
       if (editItem) {
         setEditID(id);
+        // Set the initial state for edit fields
         setName(editItem.name);
         setEmail(editItem.email);
         setRole(editItem.role);
       }
     };
   
-    const handleDelete = (id) => {
-      const updatedData = data.filter((item) => item.id !== id);
+    const handleDelete = () => {
+      // Filter out the selected items
+      const updatedData = data.filter((item) => !selectedItems.includes(item.id));
   
       setCurrentItems(updatedData);
   
+      // Update page count and current items
       const startIndex = itemOffset;
       const endIndex = startIndex + itemsPerPage;
       const currentItemsAfterDelete = updatedData.slice(startIndex, endIndex);
@@ -38,14 +53,17 @@ const Layout = (props) => {
   
       setPageCount(Math.ceil(updatedData.length / itemsPerPage));
   
+      // Adjust item offset if it exceeds the updated page count
       if (itemOffset >= pageCount * itemsPerPage) {
         setItemOffset(Math.max(0, (pageCount - 1) * itemsPerPage));
       }
   
+      // Clear selected items
+      setSelectedItems([]);
       setEditID(-1);
     };
   
-    const handleUpdate = async (id) => {
+    const handleUpdate = (id) => {
       const updatedItemIndex = data.findIndex((item) => item.id === id);
   
       if (updatedItemIndex !== -1) {
@@ -67,6 +85,7 @@ const Layout = (props) => {
   
         setPageCount(Math.ceil(updatedData.length / itemsPerPage));
   
+        // Adjust item offset if it exceeds the updated page count
         if (itemOffset >= pageCount * itemsPerPage) {
           setItemOffset(Math.max(0, (pageCount - 1) * itemsPerPage));
         }
@@ -85,6 +104,7 @@ const Layout = (props) => {
       const updatedPageCount = Math.ceil(data.length / itemsPerPage);
       setPageCount(updatedPageCount);
   
+      // Adjust item offset if it exceeds the updated page count
       if (itemOffset >= updatedPageCount * itemsPerPage) {
         setItemOffset(Math.max(0, (updatedPageCount - 1) * itemsPerPage));
       }
@@ -96,62 +116,71 @@ const Layout = (props) => {
     };
   
     return (
-        <>
-            <div style={{ margin: "10rem" }}>
-                <Table className="Table" striped bordered hover size="sm">
-                    <thead>
-                        <tr>
-                            <th><input type="checkbox" onChange={() => handleCheckboxChange()} /></th>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {currentItems.map((item) => (
-                            item.id===editID
-                            ? <tr>
-                                <td>{item.id}</td>
-                                <td><input type="text" value={name} onChange={e => setName(e.target.value)}/></td>
-                                <td><input type="text" value={email} onChange={e => setEmail(e.target.value)}/></td>
-                                <td><input type="text" value={role} onChange={e => setRole(e.target.value)}/></td>
-                                <td><button onClick={()=>handleUpdate(item.id)}>Update</button></td>
-                            </tr>
-                            :
-                            <tr key={item.id}>
-                                <td className="style"><input type="checkbox" onChange={() => handleCheckboxChange(item.id)} /></td>
-                                <td className="style">{item.id}</td>
-                                <td className="style">{item.name}</td>
-                                <td className="style">{item.email}</td>
-                                <td className="style">{item.role}</td>
-                                <td >
-                                    <Button variant="info" size="sm" onClick={() => handleEdit(item.id)}>Edit</Button>
-                                    {' '}
-                                    <Button variant="danger" size="sm" onClick={() => handleDelete(item.id)}>Delete</Button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </div>
-            <ReactPaginate
-                breakLabel="..."
-                nextLabel="next >"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={5}
-                pageCount={pageCount}
-                previousLabel="< previous"
-                renderOnZeroPageCount={null}
-                containerClassName='pagination'
-                pageLinkClassName='page-num'
-                previousLinkClassName='page-num'
-                nextLinkClassName='page-num'
-                activeLinkClassName='active'
-            />
-        </>
+      <>
+        <div style={{ margin: "10rem" }}>
+          <Button
+            variant="danger"
+            size="sm"
+            onClick={() => handleDelete()}
+            style={{ marginBottom: '10px', float: 'right' }}
+          >
+            Delete Selected
+          </Button>
+          <Table className="Table" striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>
+                  <input type="checkbox" onChange={() => handleCheckboxChange()} />
+                </th>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentItems.map((item) => (
+                item.id === editID ? (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td><input type="text" value={name} onChange={(e) => setName(e.target.value)} /></td>
+                    <td><input type="text" value={email} onChange={(e) => setEmail(e.target.value)} /></td>
+                    <td><input type="text" value={role} onChange={(e) => setRole(e.target.value)} /></td>
+                    <td><button onClick={() => handleUpdate(item.id)}>Update</button></td>
+                  </tr>
+                ) : (
+                  <tr key={item.id}>
+                    <td className="style"><input type="checkbox" onChange={() => handleCheckboxChange(item.id)} checked={selectedItems.includes(item.id)} /></td>
+                    <td className="style">{item.id}</td>
+                    <td className="style">{item.name}</td>
+                    <td className="style">{item.email}</td>
+                    <td className="style">{item.role}</td>
+                    <td>
+                      <Button variant="info" size="sm" onClick={() => handleEdit(item.id)}>Edit</Button>
+                    </td>
+                  </tr>
+                )
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          containerClassName='pagination'
+          pageLinkClassName='page-num'
+          previousLinkClassName='page-num'
+          nextLinkClassName='page-num'
+          activeLinkClassName='active'
+        />
+      </>
     )
-}
-
-export default Layout
+  }
+  
+  export default Layout;
